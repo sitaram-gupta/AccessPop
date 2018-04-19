b697340b93282bf22fd26b542e34b72e5a67bfdd
Sitaram gupta <sitaram.gupta99@gmail.com>
2018-04-19 12:57:58 +0545

    Popup view for activity with animationn

---
+++ app/src/main/java/com/example/lenovo/accesspop/Pop.java
@@ -0,0 +1,24 @@
+package com.example.lenovo.accesspop;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+
+/**
+ * Created by lenovo on 2/6/2017.
+ */
+
+public class Pop extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.popwindow);
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+        getWindow().setLayout((int) (width * .8), (int) (height * .4));
+    }
+}
